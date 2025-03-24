ae8889fa377c0c0100adeca0a4220f8b48fe4424
Layal Singh <layal.singhkolkata@gmail.com>
2025-03-24 13:02:23 -0400

    marker moves left/right (no boundaries yet)

--- game.py
+++ game.py
@@ -1,21 +1,30 @@
 import pygame
-print(pygame.__version__)
 
 # game setup
 pygame.init()
 window = pygame.display.set_mode((800, 800))
+pygame.display.set_caption('Mqix')
+
 running = True
 bg_color = (252, 215, 183)
 marker_pos = pygame.Vector2(window.get_width()/ 2, 790)
 clock = pygame.time.Clock()
-
+vel = 5
 while running:
+    pygame.time.delay(100)
+    window.fill(bg_color)
+
     for event in pygame.event.get():
         #closing the window when the close button is clicked
         if event.type == pygame.QUIT:
             running = False
 
-    window.fill(bg_color)
+    key = pygame.key.get_pressed()
+    # moves marker left and right (currently no boundaries)
+    if key[pygame.K_LEFT]:
+        marker_pos.x -= vel
+    if key[pygame.K_RIGHT]:
+        marker_pos.x += vel
 
     pygame.draw.circle(window, 'green', marker_pos, 10)
 
