c48be50ec6dc8742b3d2de3eeff1dbd14bcde10c
alexiorial <90355959+alexiorial@users.noreply.github.com>
2025-04-04 16:14:03 -0400

    started enemy.py

--- enemy.py
+++ enemy.py
@@ -1,1 +1,9 @@
+import pygame
 
+class Enemy:
+  def __init__(self, screen_width, screen_height, border, vel, diameter):
+    self.vel = vel
+    self.diameter = diameter
+
+    # start at top left corner
+    
